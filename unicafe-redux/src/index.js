4aaa43d85256fd45492e4dbeb30865fc32ddc41b
Aku <ap@Aku-MacBook-Air.local>
2023-03-02 19:20:18 +0200

    6.2

--- unicafe-redux/src/index.js
+++ unicafe-redux/src/index.js
@@ -1,5 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import counterReducer from './reducer'
+import { createStore } from 'redux'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const store = createStore(counterReducer)
+
+const App = () => {
+
+  return (
+    <div>
+        <button onClick={() => store.dispatch({type: 'GOOD'})}>good</button>
+        <button onClick={() => store.dispatch({type: 'OK'})}>ok</button>
+        <button onClick={() => store.dispatch({type: 'BAD'})}>bad</button>
+        <div>
+            good {store.getState().good}
+        </div>
+        <div>
+            ok {store.getState().ok}
+        </div>
+        <div>
+            bad {store.getState().bad}
+        </div>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+    root.render(<App />)
+  }
+
+  renderApp()
+  store.subscribe(renderApp)
